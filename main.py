3566c8b8947668b884b9ba184d3922ef5eebbf8e
GustavoFrancischini <gustavo.gofran@gmail.com>
2023-06-06 20:46:26 -0300

    inicia o programa

---
+++ main.py
@@ -0,0 +1,9 @@
+name =  input("Digite seu nome: ")
+city = input("Digite sua cidade: ")
+gender = input("Digite seu gênero: ")
+
+print(f"Seu nome é {name}")
+print(f"Sua cidade é {city}")
+print(f"Seu gênero é {gender}")
+
+print("teste")

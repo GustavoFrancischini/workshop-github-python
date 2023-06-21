85de0982127f4f8ce04e407f536a910cba873cd9
Gustavo <64756160+GustavoFrancischini@users.noreply.github.com>
2023-06-21 09:12:41 -0300

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-name =  input("Digite seu nome: ")
-city = input("Digite sua cidade: ")
-gender = input("Digite seu gênero: ")
-
-print(f"Seu nome é {name}")
-print(f"Sua cidade é {city}")
-print(f"Seu gênero é {gender}")
-
-print("teste")
